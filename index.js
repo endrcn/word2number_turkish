4537535e20fec81e6a87149461e88d74bbf9cf35
endrcnn <ender@cbot.ai>
2022-08-23 00:19:27 +0300

    magnitude geliştirmesine başlandı

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const Word2Number = require("./lib/Word2Number");
 const word2Number = new Word2Number();
 
-word2Number.convert("üç milyon beşyüz onikibin dörtyüz bir lira otuz iki kuruş");
+word2Number.convert("sekizyüz on iki milyar üç yüz kırk dört milyon beşyüz yetmişikibin dörtyüz bir lira otuz iki kuruş");
