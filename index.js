6adb4aad6100a6c204d7ec4018bbf145095fd106
endrcnn <ender@cbot.ai>
2022-08-22 22:58:15 +0300

    findNumbersIndexes method added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const Word2Number = require("./lib/Word2Number");
 const word2Number = new Word2Number();
 
-word2Number.convert("üç milyon beşyüz onikibin dörtyüz bir lira");
+word2Number.convert("üç milyon beşyüz onikibin dörtyüz bir lira otuz iki kuruş");
