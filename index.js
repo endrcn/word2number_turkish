9071e2950bea643009969ae3681fb93c3187540e
endrcnn <ender@cbot.ai>
2022-08-27 21:28:17 +0300

    telefon numarası testi eklendi

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Word2Number = require("./lib/Word2Number");
 const word2Number = new Word2Number();
 
-word2Number.convert("sekizyüz on iki milyar üç yüz kırk dört milyon beşyüz yetmişikibin dörtyüz bir lira otuz iki kuruş");
+console.log(word2Number.convert("sekizyüz on iki milyar üç yüz kırk dört milyon beşyüz yetmişikibin dörtyüz bir lira otuz iki kuruş"));
+console.log(word2Number.convert("sıfır beşyüz otuzaltı beşyüzellibeş elli beş ellibeş"));
