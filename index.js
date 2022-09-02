07f6e947feb33f26036d0c5ac1af304370dee636
endrcnn <ender@cbot.ai>
2022-09-02 22:56:51 +0300

    Zero number control has added

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ const word2Number = new Word2Number();
 // console.log(word2Number.convert("sıfır beşyüz otuzaltı beşyüzellibeş elli beş ellibeş"));
 // console.log(word2Number.convert("2 milyon 300 bin tl 290 gun"));
 // console.log(word2Number.convert("2 bin - 300 bin tl ikiyüzdoksan gun"));
-console.log(word2Number.convert("21.12.2021"));
+// console.log(word2Number.convert("21.12.2021"));
+// console.log(word2Number.convert("oniki v sıfırdokuz v ikibinondokuz v tarihli v ekstra v borcum v ne v kadar"));
+console.log(word2Number.convert("oniki v sıfırdokuz v ikibinon sıfırdokuz v tarihli v ekstra v borcum v ne v kadar"));
